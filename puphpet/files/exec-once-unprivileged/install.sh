76308b9ea948fbcf5ad7e54605ef39318c21c6d2
killbond <killbond@mail.ru>
2017-02-19 10:46:22 +0300

    global add codecept

--- puphpet/files/exec-once-unprivileged/install.sh
+++ puphpet/files/exec-once-unprivileged/install.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
 composer global require "fxp/composer-asset-plugin:^1.2.0"
+composer global require "codeception/codeception"
+
+echo "export PATH="$PATH:$HOME/.config/composer/vendor/bin"" > ~/.bashrc
